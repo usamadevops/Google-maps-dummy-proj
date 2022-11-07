d30788e125240fc34219efdc17e0ba5f1cb96286
AhsanYousaf <ahsanbhatti624@gmail.com>
2022-11-07 17:43:22 +0500

    Slot added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,6 +5,7 @@ import {
     Polygon,
 } from '@react-google-maps/api';
 import axios from 'axios';
+import Modal from '@material-ui/core/Modal';
 
 const DB_URL = 'http://localhost:3000/map';
 
@@ -30,33 +31,60 @@ function get_polygon_centroid(pts) {
 
 const Map = ({ data }) => {
     const [addCar, setaddCar] = useState([]);
-    const [cars,setCars]=useState([]);
+    const [cars, setCars] = useState([]);
+    const [buildings, setBuildings] = useState([]);
     const [delalersLocation, setDealersLocation] = useState(null);
-    const [markers, setMarkers] = useState([]);  
+    const [markers, setMarkers] = useState([]);
     const [polygonCords, setPolygonCords] = useState([]);
-    const options = useMemo(() => ({ 
+    const [submitCars, setSubmitCars] = useState([]);
+    const [car, setCar] = useState(false);
+    const [building, setBuilding] = useState(false);
+    const [mappingSlots,setMappingSlots] = useState([]);
+
+    const options = useMemo(() => ({
         clickableIcons: false
     }), [])
-    const mapRef = useRef(); 
+    const mapRef = useRef();
 
-const handlecarCoords = (e)=>{
-    setaddCar(addCar=>[...addCar,{lat:e.latLng.lat(),lng:e.latLng.lng()}]); 
-   if(addCar.length==4){
-       setCars(cars=>[...cars,addCar]);
-       alert('Car Added',cars.length);
-       setaddCar([]);
-   }
-}
+    const [open, setOpen] = useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handlecarCoords = (e) => {
+        if (addCar.length !== 4)
+            setaddCar(addCar => [...addCar, { lat: e.latLng.lat(), lng: e.latLng.lng() }]);
+        if (addCar.length === 4) {
+            setOpen(true);
+        }
+    }
+
+    const getCars = async () => {
+        const data1 = await axios.get(`${DB_URL}/${data.id}`);
+        setCars(data1?.data?.cars);
+        setBuildings(data1?.data?.buildings);
+    }
     useEffect(() => {
         setDealersLocation(() => get_polygon_centroid(data.coords));
         data.cars && setMarkers(data.cars);
         setPolygonCords(data.coords);
     }, [])
 
+    useEffect(() => {
+        getCars();
+    }, []);
+
     const onLoad = useCallback(map => (mapRef.current = map), []);
     const handleSubmitCars = async (e) => {
 
-        const data1 = await axios.put(`${DB_URL}/${data.id}`, cars);
+        console.log(buildings);
+        const data1 = await axios.patch(`${DB_URL}/${data.id}`, { "cars": cars , "buildings": buildings});
+        //const data2 = await axios.patch(`${DB_URL}/${data.id}`, { "buildings": buildings });
         if (data1) {
             alert('Cars sucessfully added!');
         }
@@ -69,8 +97,49 @@ const handlecarCoords = (e)=>{
 
     return (
         <>
+            <>
+                {
+                    open && <Modal
+                        onClose={handleClose}
+                        open={open}
+                        style={{
+                            position: 'absolute',
+                            border: '2px solid #000',
+                            backgroundColor: 'gray',
+                            boxShadow: '2px solid black',
+                            height: 100,
+                            width: 300,
+                            margin: 'auto'
+                        }}
+                    >
+                        <div>
+                            <h2>What do you want to add ?</h2>
+                            <button onClick={() => {
+                                handleClose()
+                                let brand = prompt("Please enter your car's brand", "BMW");
+                                if (brand) {
+                                    setCars(cars => [...cars, { carId: Math.random(), Brand: brand, location: addCar }]);
+                                    alert('Car Added', cars.length);
+                                    setaddCar([]);
+                                }
+                            }}>Car</button>
+                            <button onClick={() => {
+                                handleClose()
+                                let name = prompt("Please enter your building name", "Office");
+                                if (name) {
+                                    setBuildings(buildings => [...buildings, { buildingId: Math.random(), Name: name, location: addCar }]);
+                                    alert('Building Added', buildings.length);
+                                    console.log(addCar)
+                                    setaddCar([]);
+                                    console.log(buildings);
+                                }
+                            }}>Building</button>
+                        </div>
+                    </Modal>
+                }
+            </>
             <div>
-                <button onClick={handleSubmitCars} style={{color:'white',padding:'10px',position:'absolute',width:'100%',backgroundColor:'chocolate',zIndex:1000,bottom:40,right:20}}>Submit Cars</button>
+                <button onClick={handleSubmitCars} style={{ color: 'white', padding: '10px', position: 'absolute', width: '100%', backgroundColor: 'chocolate', zIndex: 1000, bottom: 40, right: 20 }}>Submit Cars</button>
             </div>
             <div className="map">
                 {delalersLocation &&
@@ -87,17 +156,18 @@ const handlecarCoords = (e)=>{
                             {
                                 addCar &&
                                 <Polygon path={addCar}
-                                key={addCar.lat}
-                                options={{
-                                    strokeColor: "#FF0000",
-                                    fillOpacity: 1
-                                }}
-                            />
+                                    key={addCar.lat}
+                                    options={{
+                                        strokeColor: "#FF0000",
+                                        fillOpacity: 1
+                                    }}
+                                />
                             }
                             {
-                               cars && cars.map(item =>
-                                    <Polygon paths={item}
-                                    key={item.lat}
+                                cars && cars.map(item =>
+                                    <Polygon paths={item?.location}
+                                        key={item?.location.lat}
+                                        label={"Car"}
                                         options={{
                                             strokeColor: "#FF0000",
                                             fillOpacity: 2
@@ -105,13 +175,24 @@ const handlecarCoords = (e)=>{
                                     />)
                             }
 
+{
+                                buildings && buildings.map(item =>
+                                    <Polygon paths={item?.location}
+                                        key={item?.location.lat}
+                                        options={{
+                                            strokeColor: "#FFF000",
+                                            fillOpacity: 2
+                                        }}
+                                    ></Polygon>)
+                            }
+
                             <Polygon paths={polygonCords}
                                 options={{
                                     strokeColor: "#FFffff",
-                                    strokeWeight:4,
+                                    strokeWeight: 4,
                                     fillOpacity: 0
                                 }}
-                                onClick={(e) => {handlecarCoords(e)}}
+                                onClick={(e) => { handlecarCoords(e) }}
                             />
                         </>
                     </GoogleMap>
